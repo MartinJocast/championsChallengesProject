062fe7a69e6f5ad0b75b5942bd5572222b4ab626
IN-LL-034 <IN-LL-034@IN-LL-034>
2019-09-05 15:56:29 +0200

    Commit UserDao

---
+++ src/main/java/fr/formation/inti/dao/UserDao.java
@@ -0,0 +1,10 @@
+package fr.formation.inti.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.formation.inti.entities.Users;
+
+public interface UserDao extends JpaRepository<Users, Integer>{
+
+	
+}
