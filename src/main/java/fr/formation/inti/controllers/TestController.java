3fe8fbe7b70c34d9a5fefd4ff00561ba8ceab2a1
AlexandreP8 <alpi0512@gmail.com>
2019-09-05 10:17:09 +0200

    commit web

---
+++ src/main/java/fr/formation/inti/controllers/TestController.java
@@ -0,0 +1,19 @@
+package fr.formation.inti.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	
+	@RequestMapping("/home")
+	public String index(Model model) {
+
+		return "views/home";
+	}
+	
+
+	
+}
